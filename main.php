784e1e57a06e5509e2d862f119684f89131f7dae
Keanu M <113337515+M-Keanu@users.noreply.github.com>
2024-03-04 12:35:09 +0100

    4-3-2024 (12:35)

--- main.php
+++ main.php
@@ -13,19 +13,33 @@ $inbox = imap_open($hostname, $username, $password) or die('Cannot connect to ma
 //Fetch e-mails
 $emails = imap_search($inbox, 'ALL');
 
-foreach ($emails as $email_number) {
-    $overview = imap_fetch_overview($inbox, $email_number, 0);
-    echo 'Subject: ' . $overview[0]->subject . '<br>';
-}
+// foreach ($emails as $email_number) {
+//     $overview = imap_fetch_overview($inbox, $email_number, 0);
+//     echo 'Subject: ' . $overview[0]->subject . '<br>';
+//     echo "From: " . $overview->from[0] . '<br>';
+//     echo "Message: " . $overview->message[0] . '<br><br>';
+// }
+
+if ($emails) 
+{
+    $output = '';
+    foreach ($emails as $email_number) {
+        $header = imap_headerinfo($inbox, $email_number);
+        $from = $header->from;
+        $output .= 'Subject: ' . $header->subject . '<br>';
+        $output .= "From: " . $from_email . '<br>';
+        $output .= "Message: " . $header->text . '<br><br>';
+    }
+    echo $output;
+} 
 
 imap_close($inbox);
+
 
 //Send e-mail
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
-
-//require 'vendor/autoload.php';
 
 $mail = new PHPMailer(true);
 
@@ -42,7 +56,7 @@ try {
     $mail->addAddress('vista.challenge@gmx.net', 'Send mail test');
 
     $mail->isHTML(true);
-    $mail->Subject = 'Send test mail';
+    $mail->Subject = 'Test mail';
     $mail->Body    = 'Dit is een test e-mail';
 
     $mail->send();
@@ -50,6 +64,5 @@ try {
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-
 
 ?>
